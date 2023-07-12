ed876d389933f6507c644968b716c4370c83cc53
Jonatan Marques <jonatanmarsv@gmail.com>
2023-07-12 18:57:56 -0300

    exercicio 11 concluido

--- testArea/cursos/CJRM roger-melo/exercicio-11/app.js
+++ testArea/cursos/CJRM roger-melo/exercicio-11/app.js
@@ -5,37 +5,61 @@
   - Ela deve ter um escopo global.
 */
 
+const nome = 'Jonatan'
+
 /*
   02
 
-  - Funções também são blocos de código;
-  - Crie uma função que ao ser invocada, exibe sua idade no console;
-  - Dentro da função, declare uma let que armazena a sua idade;
-  - Exiba sua idade no console, através da invocação da função;
+  - Funções também são blocos de código; ✅
+  - Crie uma função que ao ser invocada, exibe sua idade no console;✅
+  - Dentro da função, declare uma let que armazena a sua idade;✅
+  - Exiba sua idade no console, através da invocação da função;✅
   - Do lado de fora da função exiba no console o valor da let que foi declarada  
-    dentro da função;
+    dentro da função;✅
   - Um erro com a mensagem "Uncaught ReferenceError: SUA_VARIÁVEL is not  
-    defined" será exibido no console;
-  - Você sabe por que isso aconteceu?
+    defined" será exibido no console;✅
+  - Você sabe por que isso aconteceu?✅
 */
+
+const age = num => {
+  let idade = num
+  return idade
+}
+//console.log(age(28))
+//console.log(idade) // POR QUE LET SÓ É ACESIVEL DENTRO DO ESCOPO, ESTA PRIVADO PARA SOMENTE ESCOPO DA DETERMINA FUNÇÃO
 
 /*
   03
 
   - Crie um objeto "car" com as seguintes propriedades e métodos:
-    - name, que recebe o nome do carro;
-    - brand, que recebe a marca do carro;
-    - colors, que recebe 3 cores para o carro;
-    - isRunning, que recebe um boolean indicando se o carro está em movimento.  
+    - name, que recebe o nome do carro;✅
+    - brand, que recebe a marca do carro;✅
+    - colors, que recebe 3 cores para o carro;✅
+    - isRunning, que recebe um boolean indicando se o carro está em movimento.✅  
       Inicialmente, deve receber um boolean indicando que o carro não está em  
-      movimento;
+      movimento;✅
     - run, que é um método que faz o carro andar e retorna a mensagem  
-      "O NOME_DO_CARRO está em movimento";
+      "O NOME_DO_CARRO está em movimento";✅
     - stop, que é um método que faz o carro parar e retorna a mensagem  
-      "O NOME_DO_CARRO está parado";
+      "O NOME_DO_CARRO está parado";✅
     - getColorsMessage, que é um método que retorna a mensagem  
-      "O NOME_DO_CARRO está disponível nas cores COR_01, COR_02 e COR_03".
+      "O NOME_DO_CARRO está disponível nas cores COR_01, COR_02 e COR_03".✅
 */
+const car = {
+  name: 'PORTOFINO',
+  brand: 'Ferrari',
+  colors: ['vermelho', 'branco', 'preto'],
+  isRunning: false,
+  run: function () {
+    this.isRunning = true
+    return `O ${this.name} está em movimento`
+  },
+  stop: function () {
+    this.isRunning = false
+    return `O ${this.name} está parado`
+  },
+  getColorsMessage: function () { return `O ${this.name} está disponível nas cores ${this.colors[0]}, ${this.colors[1]} e ${this.colors[2]}` },
+}
 
 /*
   04
@@ -43,11 +67,14 @@
   - Faça o carro andar e exiba no console se ele realmente está em movimento.
 */
 
+// console.log(car.run())
+
 /*
   05
 
   - Faça o carro parar e exiba no console se ele realmente está parado.
 */
+// console.log(car.stop())
 
 /*
   06
@@ -55,9 +82,12 @@
   - Exiba, no console, a mensagem com as cores do carro.
 */
 
+//console.log(car.getColorsMessage())
+
 /*
   07
 
   - Exiba, no console, a mensagem "O carro é um MARCA_DO_CARRO NOME_DO_CARRO";
   - Utilize a notação de colchetes para acessar as propriedades do carro.
 */
+console.log(`O carro é um ${car.brand} ${car.name}`)

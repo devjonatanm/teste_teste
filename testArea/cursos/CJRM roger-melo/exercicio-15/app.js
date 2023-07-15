76a48a94539d78aa819c4c203e246645cf3aafa6
Jonatan Marques <jonatanmarsv@gmail.com>
2023-07-14 22:41:07 -0300

    bateria 15 de exercicios concluida

--- testArea/cursos/CJRM roger-melo/exercicio-15/app.js
+++ testArea/cursos/CJRM roger-melo/exercicio-15/app.js
@@ -5,8 +5,10 @@
     uma classe 'video';
   - Exiba no console os elementos filhos da ul com a classe já inserida.
 */
-
-
+const li = document.querySelectorAll('li')
+for (let i = 0; i < document.querySelectorAll('li').length; i++) {
+  document.querySelectorAll('li')[i].classList.add('video')
+}
 
 /*
   02
@@ -14,23 +16,21 @@
   - Usando a propriedade adequada, descubra quem é o elemento pai do h2
     e exiba-o no console;
 */
-
-
+console.log(document.querySelector('h2').parentElement)
 
 /*
   03
 
   - Descubra quem é o próximo elemento irmão do h1 e exiba-o no console;
 */
-
-
+console.log(document.querySelector('h1').nextElementSibling)
 
 /*
   04
 
   - Descubra quem é o irmão anterior da ul e exiba-o no console;
 */
-
+console.log(document.querySelector('ul').previousElementSibling)
 
 
 /*
@@ -40,6 +40,11 @@
     exibida no console.
 */
 
+for (let i = 0; i < document.querySelectorAll('li').length; i++) {
+  document.querySelectorAll('li')[i].addEventListener('click', e => {
+    return console.log(document.querySelectorAll('li')[i])
+  })
+}
 
 
 /*
@@ -60,9 +65,28 @@ const videos = [{
   length: '00:02:55'
 }]
 
+const button = document.querySelector('button')
+let contador = 0
+button.addEventListener('click', e => {
+  const ul = document.querySelector('ul')
+
+  if (videos[contador] !== undefined) {
+    const li_add = document.createElement('li') // criando elemento
+    li_add.innerHTML = `<li class="video">${videos[contador].name}</li>`
+    ul.appendChild(li_add)
+    contador++;
+  }
+})
+
 /*
   07
 
   - Se um clique no h1 acontecer, faça com que todos os elementos dentro do body 
     sejam removidos.
 */
+
+const h1 = document.querySelector('h1')
+const body = document.querySelector('body')
+h1.addEventListener('click', event => {
+  return body.innerHTML = '' // REMOVE TODOS ELEMENTOS FILHOS DE BODY
+})

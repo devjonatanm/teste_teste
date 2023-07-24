9e7cd0420d3881cef47d92b7efe4bff63871aba0
Jonatan Marques <jonatanmarsv@gmail.com>
2023-07-24 15:04:10 -0300

    bateria 27 concluida

--- testArea/cursos/CJRM roger-melo/exercicio-27/app.js
+++ testArea/cursos/CJRM roger-melo/exercicio-27/app.js
@@ -71,7 +71,7 @@ let car = { color: 'amarelo' }
 //car = { color: 'azul' }
 car = { ...car, color: 'azul' }
 
-console.log(car)
+// console.log(car)
 
 
 
@@ -80,14 +80,20 @@ console.log(car)
 
   - Crie uma função que recebe 3 argumentos; ✅
   - Se um dos 3 argumentos não for passado na invocação, a string 'A função 
-    deve ser invocada com 3 argumentos' deve ser retornada;
+    deve ser invocada com 3 argumentos' deve ser retornada; ✅
   - Se todos os argumentos forem passados, retorne a string 'A função foi 
-    invocada com 3 argumentos'.
+    invocada com 3 argumentos'. ✅
 */
-const recebTres = (x,y,z) => {
 
+const recebTres = (x, y, z) => {
+  if ([x, y, z].every(x => x === undefined)) {
+    return 'A função deve ser invocada com 3 argumentos'
+  } else if ([x, y, z].every(x => x !== undefined)) {
+    return 'A função foi invocada com 3 argumentos'
+  }
 }
-
+// console.log(recebTres(2,3,2))
+// console.log(recebTres())
 
 
 /*
@@ -95,24 +101,50 @@ const recebTres = (x,y,z) => {
 
   - O objeto abaixo representa uma caixa de livros com espaço para 5 livros e 
     que, no momento em que foi declarado, possui nenhum livro dentro;
-  - Crie um método que irá adicionar livros na caixa;
+  - Crie um método que irá adicionar livros na caixa;✅
   
   Este método deve:
     - Receber por parâmetro o número de livros que serão colocados na caixa. 
       Esse número não precisa encher a caixa de uma só vez, os livros podem 
-      ser acrescentados aos poucos;
-    - Retornar a frase: "Já há 'X' livros na caixa";
+      ser acrescentados aos poucos; ✅
+    - Retornar a frase: "Já há 'X' livros na caixa"; ✅
     - Se a caixa já estiver cheia, com todos os espaços já preenchidos, o 
-      método deve retornar a frase: "A caixa já está cheia";
+      método deve retornar a frase: "A caixa já está cheia";✅
     - Se ainda houverem espaços na caixa mas a quantidade de livros passada por
       parâmetro for ultrapassar o limite de espaços da caixa, mostre quantos 
       espaços ainda podem ser ocupados, com a frase: "Só cabem mais 
-      QUANTIDADE_DE_LIVROS_QUE_CABEM livros";
+      QUANTIDADE_DE_LIVROS_QUE_CABEM livros";✅
     - Se couber somente mais um livro, mostre a palavra "livro" (no singular) 
-      na frase acima.
+      na frase acima.✅
 */
 
 let booksBox = {
   spaces: 5,
-  booksIn: 0
+  booksIn: 0,
+  add(n) {
+    if (this.spaces <= 0) {
+      return 'A caixa já está cheia'
+    }
+    if (n > this.spaces && this.spaces === 1) {
+      return `Só cabem mais ${this.spaces} livro`
+    } else if (n > this.spaces) {
+      return `Só cabem mais ${this.spaces} livros`
+    }
+    if (n !== undefined) {
+      this.spaces = this.spaces - n
+      this.booksIn = this.booksIn + n
+      return `Já há ${Math.abs(this.spaces - 5)} livros na caixa`
+    } else {
+      --this.spaces
+      ++this.booksIn
+      return `Já há ${Math.abs(this.spaces - 5)} livros na caixa`
+    }
+  }
 }
+
+booksBox.add()
+booksBox.add()
+booksBox.add()
+booksBox.add()
+
+console.log(booksBox.add(7))
